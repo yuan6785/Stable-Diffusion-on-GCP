799c4d72457fb7d640a00f2f1a610714858b0378
mac yx <fsdf@f.com>
2023-04-20 17:12:57 +0800

    1

---
+++ Stable-Diffusion-UI-Novel/docker_inference/docker_data/start_pro.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 用法参考有道搜索 [linux有用的fg命令]  
+set -m
+echo PYTHONUNBUFFERED=$PYTHONUNBUFFERED
+echo PROJECT_ENV=$PROJECT_ENV
+while true;do date;sleep 86400;date;echo "loop--";done &
+/root/miniconda3/envs/sd_python310/bin/python launch.py --listen --xformers --medvram &
+/usr/bin/supervisord -c /etc/supervisor.conf &
+tail -f /var/log/loopscript.log &
+tail -f /var/log/sdwebui.log &
+fg %1
--- Stable-Diffusion-UI-Novel/docker_inference/readme.txt
+++ Stable-Diffusion-UI-Novel/docker_inference/readme.txt
@@ -1,8 +1,8 @@
 用anconda版本的dockerfile
 记得修改deployment.yaml中的挂载位置
 
-最终用的是
+最终用的是(再加个循环任务)
 Dockerfile.root.ancondapython
 
 但是:
-Dockerfile.root.ancondapython.supervisor 也非常重要----非常有参考价值，有nginx代理sdwebui，supervisor管理多个进程，循环任务等等
+Dockerfile.root.ancondapython.nginx.supervisor.yx 超级重要(虽然没有在线上用)----非常有参考价值，有nginx代理sdwebui，supervisor管理多个进程，循环任务等等
