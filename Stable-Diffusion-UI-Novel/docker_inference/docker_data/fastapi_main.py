411648725823dca6dc4fde17085f7e2492fc2a38
mac yx <fsdf@f.com>
2023-04-20 16:53:28 +0800

    1

---
+++ Stable-Diffusion-UI-Novel/docker_inference/docker_data/fastapi_main.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+import os
+import asyncio
+app = FastAPI()
+
+
+async def tail_file(file_path: str, websocket: WebSocket):
+    with open(file_path, "r") as f:
+        # Seek to the end of the file
+        f.seek(0, os.SEEK_END)
+
+        while True:
+            line = f.readline()
+            if line:
+                await websocket.send_text(line)
+            else:
+                # Sleep for a short interval to avoid CPU hogging
+                await asyncio.sleep(0.1)
+
+
+@app.get("/wqipqniwalala/restart_server")
+async def root():
+    os.system("supervisorctl restart sdwebui")
+    return {"message": "restart success"}
+
+
+@app.get("/wqipqniwalala/sdwebuilogs")
+# 从url读取get参数filename
+async def get_file(filename: str=None, count: int=200):
+    # 读取文件内容
+    if filename is None:
+        filepath = "/var/log/sdwebui.log"
+    else:
+        filepath = f"/var/log/{filename}.log"
+    # 读取最后count行,html 换行格式化返回
+    if os.path.exists(filepath):
+        with open(filepath, "r") as f:
+            lines = f.readlines()
+            lines = lines[-count:]
+            lines = "<br/>".join(lines)
+            return HTMLResponse(content=lines)
+    else:
+        return {"message": "file not exist"}
+
+
+
+
+    
--- Stable-Diffusion-UI-Novel/docker_inference/docker_data/start.sh
+++ Stable-Diffusion-UI-Novel/docker_inference/docker_data/start.sh
@@ -4,6 +4,7 @@ set -m
 echo PYTHONUNBUFFERED=$PYTHONUNBUFFERED
 echo PROJECT_ENV=$PROJECT_ENV
 while true;do date;sleep 86400;date;echo "loop--";done &
-/root/miniconda3/envs/sd_python310/bin/python launch.py --port 9965 --listen --xformers --medvram &
 /usr/bin/supervisord -c /etc/supervisor.conf &
+tail -f /var/log/loopscript.log &
+tail -f /var/log/sdwebui.log &
 fg %1
