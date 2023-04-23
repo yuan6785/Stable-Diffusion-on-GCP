0de3f237e7f1f411656e3623183a9bfcd008e2d5
mac yx <fsdf@f.com>
2023-04-23 18:03:57 +0800

    1

--- Stable-Diffusion-UI-Novel/yuanxiao_kubernetes/0_public_ubuntu_server/install_vsftp.sh
+++ Stable-Diffusion-UI-Novel/yuanxiao_kubernetes/0_public_ubuntu_server/install_vsftp.sh
@@ -1,5 +1,6 @@
 apt update
-apt -y install vsftpd
+apt -y install vsftpd db-util
+mkdir -p /var/run/vsftpd/empty
 echo $(vsftpd -v)
 echo "local_enable=YES
 pam_service_name=vsftpd
@@ -11,10 +12,11 @@ guest_username=FTP
 user_config_dir=/etc/vsftpd_yx/vuser_conf/
 allow_writeable_chroot=YES
 chroot_local_user=YES
+secure_chroot_dir=/var/run/vsftpd/empty
 pasv_enable=YES
 pasv_min_port=20000
 pasv_max_port=20045">/etc/vsftpd.conf
-mkdir  /etc/vsftpd_yx
+mkdir -p /etc/vsftpd_yx
 cd  /etc/vsftpd_yx
 echo "playdayy
 1
@@ -23,8 +25,13 @@ pla12345">/etc/vsftpd_yx/vuser
 cd  /etc/vsftpd_yx&&db_load -T -t hash -f vuser vuser.db
 echo "auth       required     /usr/lib/x86_64-linux-gnu/security/pam_userdb.so   db=/etc/vsftpd_yx/vuser 
 account    required     /usr/lib/x86_64-linux-gnu/security/pam_userdb.so   db=/etc/vsftpd_yx/vuser">/etc/pam.d/vsftpd
-cd  /etc/vsftpd_yx&&mkdir vuser_conf&&cd /etc/vsftpd_yx/vuser_conf/
-useradd  --create-home -s  /sbin/nologin FTP
+cd  /etc/vsftpd_yx&&mkdir -p vuser_conf&&cd /etc/vsftpd_yx/vuser_conf/
+userisexist=$(id -u FTP >/dev/null 2>&1 || echo "User does not exist")
+if [[ ! -z "${userisexist}" ]]; then
+    useradd --create-home -s /sbin/nologin FTP
+else
+    echo "User already exists"
+fi
 runuser FTP -s /bin/bash -c 'mkdir -p /home/FTP/playdayy&&echo "1" > /home/FTP/playdayy/1.txt' 
 echo "local_root=/home/FTP/playdayy
 write_enable=YES
@@ -38,4 +45,8 @@ anon_upload_enable=YES
 anon_mkdir_write_enable=YES
 anon_other_write_enable=YES
 anon_umask=000">/etc/vsftpd_yx/vuser_conf/playdayy_admin
-nohup /usr/sbin/vsftpd /etc/vsftpd.conf &
+# 先杀死vsftpd进程,先找出进程号,然后杀死进程，一句代码执行, 进程不存不要报错
+# ps -ef|grep vsftpd|grep -v grep|awk '{print $2}'|xargs kill -9
+# 启动vsftp
+# nohup /usr/sbin/vsftpd /etc/vsftpd.conf &
+echo "end"
--- Stable-Diffusion-UI-Novel/yuanxiao_kubernetes/0_public_ubuntu_server/readme.txt
+++ Stable-Diffusion-UI-Novel/yuanxiao_kubernetes/0_public_ubuntu_server/readme.txt
@@ -1,2 +1,59 @@
 vsftpd的虚拟用户配置参考笔记【vsftp配置虚拟用户访问】
 
+
+
+
+
+您可以通过以下步骤在 GKE 上创建 Service 和 Ingress 来公开 vsftpd：
+
+    创建一个 Kubernetes Service，该服务将暴露 vsftpd Pod 的端口。可以使用以下 YAML 文件创建一个 Service：
+
+yaml
+
+apiVersion: v1
+kind: Service
+metadata:
+  name: vsftpd-service
+spec:
+  selector:
+    app: vsftpd
+  ports:
+    - name: ftp
+      protocol: TCP
+      port: 21
+      targetPort: 21
+    - name: passive-ports
+      protocol: TCP
+      port: 20000-20045
+      targetPort: 20000-20045
+  type: ClusterIP
+
+    创建一个 Kubernetes Ingress，该 Ingress 将将外部流量路由到该 Service。可以使用以下 YAML 文件创建一个 Ingress：
+
+yaml
+
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: vsftpd-ingress
+spec:
+  rules:
+    - host: vsftpd.example.com # 将此值更改为您的域名
+      http:
+        paths:
+          - path: /
+            pathType: Prefix
+            backend:
+              service:
+                name: vsftpd-service
+                port:
+                  name: ftp
+
+    部署和启动 Service 和 Ingress：
+
+ruby
+
+$ kubectl apply -f vsftpd-service.yaml
+$ kubectl apply -f vsftpd-ingress.yaml
+
+这将创建一个 Service 和 Ingress，以公开 vsftpd Pod 的端口。在 Ingress 创建后，您需要将 vsftpd.example.com 添加到您的 DNS 服务器中，并使用 ftp://vsftpd.example.com 连接到 vsftpd 服务器。
