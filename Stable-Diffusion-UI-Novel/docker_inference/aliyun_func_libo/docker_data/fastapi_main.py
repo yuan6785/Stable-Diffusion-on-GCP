6accc2df80aa42cb1ee485aff68c18f27005981e
mac yx <fsdf@f.com>
2023-06-13 18:29:17 +0800

    1

--- Stable-Diffusion-UI-Novel/docker_inference/aliyun_func_libo/docker_data/fastapi_main.py
+++ Stable-Diffusion-UI-Novel/docker_inference/aliyun_func_libo/docker_data/fastapi_main.py
@@ -8,6 +8,15 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    """
+    @des: 
+    重要文件，解决sdwebui放在后端或者supervisor里面启动不了的问题；
+    当云函数启动后，只有在访问云函数期间，cpu才不会被冻结，所以这里的做法是云函数启动后
+    访问根url，然后等待120秒(这期间cpu不会被冻结)，
+    这样sdwebui就有足够的时间启动了，然后再返回html，这样就可以
+
+    云函数并发度设置为： 500
+    """
     await asyncio.sleep(120) # 120秒够启动sd了
     # 返回html
     return HTMLResponse(content="""
@@ -16,7 +25,7 @@ async def root():
             <title>starting sd ....</title>
         </head>
         <body>
-            <h1>200,请耐心等待sdwebui服务器启动(大概2-4分钟)...</h1>
+            <h1>200,请耐心等待sdwebui服务器启动(大概2-4分钟)...刷新浏览器</h1>
         </body>
     </html>
     """, status_code=200)
