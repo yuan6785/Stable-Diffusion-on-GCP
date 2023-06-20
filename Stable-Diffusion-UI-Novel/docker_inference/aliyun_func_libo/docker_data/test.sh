cc4e0e73ab8555ee487b473ca929c52cc69ef371
mac yx <fsdf@f.com>
2023-06-20 16:37:29 +0800

    1

--- Stable-Diffusion-UI-Novel/docker_inference/aliyun_func_libo/docker_data/test.sh
+++ Stable-Diffusion-UI-Novel/docker_inference/aliyun_func_libo/docker_data/test.sh
@@ -7,3 +7,5 @@ while getopts ":d:s:" opt;do case $opt in d) aa=2;; s) aa=3;; ?) aa=4;; esac don
 echo "shell脚本打印: $aa"
 python -c "print('python打印:$aa')"
 bash -c "echo bash打印:$aa"
+# 判断aa不等于abcd,则输出1,否则输出2
+if [ $aa != "abcd" ];then echo "不等于"; else echo "等于"; fi
