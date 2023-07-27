84c5df1bd9907a85aa35cb3d06c1451ec5a2b0d0
mac yx <fsdf@f.com>
2023-07-27 18:14:54 +0800

    1

--- Stable-Diffusion-UI-Novel/docker_inference/aliyun_func_libo/docker_data/start_finally_20230713.sh
+++ Stable-Diffusion-UI-Novel/docker_inference/aliyun_func_libo/docker_data/start_finally_20230713.sh
@@ -18,7 +18,7 @@ if [ ! -d "/home/stable-diffusion-webui/modules" ]; then rsync -azP --no-perms -
 echo ---------start ln base-------$(date +"%Y-%m-%d %H:%M:%S")---------------
 if [ ! -d "/home/stable-diffusion-webui/models" ]; then mkdir /home/stable-diffusion-webui/models; fi
 for dir in Codeformer deepbooru ESRGAN GFPGAN  karlo LDSR SwinIR VAE-approx; do if [ ! -L "/home/stable-diffusion-webui/models/$dir" ]; then ln -s /share/sdwebui_public/versions/sdwebui_env/stable-diffusion-webui-20230713/stable-diffusion-webui/models/$dir  /home/stable-diffusion-webui/models/; fi; done
-for dir in ControlNet  hypernetworks  Lora  Stable-diffusion  VAE; do if [ ! -L "/home/stable-diffusion-webui/models/$dir" ]; then ln -s /share/sdwebui_public/public/models/$dir  /home/stable-diffusion-webui/models/; fi; done
+for dir in ControlNet  hypernetworks  Lora  Stable-diffusion  VAE BLIP torch_deepdanbooru; do if [ ! -L "/home/stable-diffusion-webui/models/$dir" ]; then ln -s /share/sdwebui_public/public/models/$dir  /home/stable-diffusion-webui/models/; fi; done
 for dir in embeddings  localizations  outputs  samples  scripts; do if [ ! -L "/home/stable-diffusion-webui/$dir" ]; then ln -s /share/sdwebui_public/public/$dir  /home/stable-diffusion-webui/; fi; done
 # echo ---------start ln additional networks-------$(date +"%Y-%m-%d %H:%M:%S")---------------
 # rm -rf /home/stable-diffusion-webui/extensions/sd-webui-additional-networks/models/lora/*
