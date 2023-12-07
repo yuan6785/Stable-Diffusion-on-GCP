ea427ede6b1bb115bd9b719ab880834d172b031a
mac yx <fsdf@f.com>
2023-12-07 15:42:11 +0800

    1

--- Stable-Diffusion-UI-Novel/docker_inference/aliyun_ecs_sd/1_4_1/ecs_init.sh
+++ Stable-Diffusion-UI-Novel/docker_inference/aliyun_ecs_sd/1_4_1/ecs_init.sh
@@ -1,4 +1,5 @@
 # /mnt/sdwebui_public/ecs_versions/ecs_init.sh  # 记得chmod +x /mnt/sdwebui_public/ecs_versions/ecs_init.sh 
 echo "ecs init inner start"
 cd /home; rm -rf stable-diffusion-webui; mkdir -p stable-diffusion-webui
+cd /home; rm -rf ComfyUI; mkdir -p ComfyUI
 echo "ecs init inner end"
