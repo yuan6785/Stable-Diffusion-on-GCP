da932e3d0a939b4828192071bca37310114ac14a
mac yx <fsdf@f.com>
2023-12-07 18:03:55 +0800

    1

--- Stable-Diffusion-UI-Novel/docker_inference/aliyun_ecs_sd/1_4_1/ecs_pre_start_comfiui_20231205.sh
+++ Stable-Diffusion-UI-Novel/docker_inference/aliyun_ecs_sd/1_4_1/ecs_pre_start_comfiui_20231205.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 # /mnt/sdwebui_public/ecs_versions/1_4_1/ecs_pre_start.sh  #记得 chmod +x /mnt/sdwebui_public/ecs_versions/1_4_1/ecs_pre_start.sh
+# comfyui的版本构建参考笔记[comfyui的安装]
 echo ---------start-------$(date +"%Y-%m-%d %H:%M:%S")---------------
 # sleep 10 && \
 echo ---------start set env-------$(date +"%Y-%m-%d %H:%M:%S")---------------
@@ -12,6 +13,33 @@ echo ---------start ln base-------$(date +"%Y-%m-%d %H:%M:%S")---------------
 if [ ! -L "/home/ComfyUI/models/checkpoints/Stable-diffusion" ]; then
     ln -s /mnt/sdwebui_public/public/models/Stable-diffusion /home/ComfyUI/models/checkpoints/
 fi
+if [ ! -L "/home/ComfyUI/models/controlnet/ControlNetNormal" ]; then
+    ln -s /mnt/sdwebui_public/public/models/ControlNet/ControlNetNormal  /home/ComfyUI/models/controlnet/
+fi
+if [ ! -L "/home/ComfyUI/models/controlnet/ControlNetXL" ]; then
+    ln -s /mnt/sdwebui_public/public/models/ControlNet/ControlNetXL  /home/ComfyUI/models/controlnet/
+fi
+if [ ! -L "/home/ComfyUI/models/vae/VAE" ]; then
+    ln -s /mnt/sdwebui_public/public/models/VAE  /home/ComfyUI/models/vae/
+fi
+if [ ! -L "/home/ComfyUI/models/hypernetworks/hypernetworks" ]; then
+    ln -s /mnt/sdwebui_public/public/models/hypernetworks  /home/ComfyUI/models/hypernetworks/
+fi
+if [ ! -L "/home/ComfyUI/models/embeddings/embeddings" ]; then
+    ln -s /mnt/sdwebui_public/public/embeddings  /home/ComfyUI/models/embeddings/
+fi
+if [ ! -L "/home/ComfyUI/models/clip_vision/clip_vision" ]; then
+    ln -s /mnt/sdwebui_public/public/models/clip_vision  /home/ComfyUI/models/clip_vision
+fi
+clip_vision
+if [ ! -L "/home/ComfyUI/models/loras/Lora" ]; then
+    ln -s /mnt/sdwebui_public/public/models/Lora  /home/ComfyUI/models/loras/
+fi
+# 这个先不做,因为不清楚文件名格式,害怕越来越多
+# if [ ! -L "/home/ComfyUI/output" ]; then
+#     ln -s /mnt/sdwebui_public/public/outputs/comfyui  /home/ComfyUI/output
+# fi
+/mnt/sdwebui_public/public/outputs
 echo ---------end pre-------$(date +"%Y-%m-%d %H:%M:%S")---------------
 echo ---------start launch pre-------$(date +"%Y-%m-%d %H:%M:%S")---------------
 echo ---------start sd server-------$(date +"%Y-%m-%d %H:%M:%S")---------------
