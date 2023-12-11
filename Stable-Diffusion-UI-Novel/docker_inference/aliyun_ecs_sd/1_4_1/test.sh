a1c63c3973db5c67fc2eede064629fb563a2264a
mac yx <fsdf@f.com>
2023-12-11 14:54:36 +0800

    1

--- Stable-Diffusion-UI-Novel/docker_inference/aliyun_ecs_sd/1_4_1/test.sh
+++ Stable-Diffusion-UI-Novel/docker_inference/aliyun_ecs_sd/1_4_1/test.sh
@@ -1,7 +1,15 @@
-a="fdacomfyuifds"
-# 是否包含comfyui bash shell
-if [[ $a =~ "comfyui" ]]; then
-    echo "包含comfyui"
-else
-    echo "不包含comfyui"
-fi
+max_attempts=20 # 最多重试20次
+ecs_version=null # 预设为null
+i=1
+while [ $i -le $max_attempts ]; do
+    if [ -f /var/lib/cloud/instance/user-data.txt ]; then
+        ecs_version=$(jq -r '.version' /var/lib/cloud/instance/user-data.txt)
+        if [ "$ecs_version" = "null" ]; then
+            echo "获取到 ecs_version 有值 $ecs_version";
+            break
+        fi
+    fi
+    echo "尝试 #$i: user-data.txt 不存在或未找到版本信息，正在重试...";
+    sleep 5
+    i=$((i+1))
+done
